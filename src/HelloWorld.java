01b85ad0835a1afaf9dbb9a546becb499067304c
Tu Dang <tudang87@gmail.com>
2013-03-24 15:37:38 +0100

    Rev.1 Hello World implemented

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tu Huynh Dang
+ * Date: 3/24/13
+ * Time: 3:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
